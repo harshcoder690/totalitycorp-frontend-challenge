dfbc19f950ed4cc9fe9b259d34b4c8ca4bab55af
HARSH JAIN <71275600+harshcoder690@users.noreply.github.com>
2023-09-22 16:45:01 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import Header from "./components/Header";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+import Address from "./components/Address";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <div className="App">
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/address">
+          <Address />
+        </Route>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
